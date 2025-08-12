d92500980f41a9c39cc9b1ce50ff68a4c49337cd
Trevinsz <produtoraindi768@gmail.com>
2025-08-12 16:40:35 -0300

    Update page.tsx

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,15 +1,11 @@
 import HeroSection from "@/components/sections/HeroSection"
 import { NewsSection } from "@/components/sections/NewsSection"
 import { StreamersSection } from "@/components/sections/StreamersSection"
-import { FeaturedMatchesSection } from "@/components/sections/FeaturedMatchesSection"
 import { TournamentsSection } from "@/components/sections/TournamentsSection"
 
 export default function Home() {
   return (
     <>
-      <section id="matches">
-        <FeaturedMatchesSection />
-      </section>
       <section id="hero">
         <HeroSection />
       </section>
